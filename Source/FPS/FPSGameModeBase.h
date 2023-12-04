d62961e3d6c94d605e7735be2e024545f39ce304
Ryutaro Yamakawa <leokun0210@gmail.com>
2023-12-05 00:23:59 +0900

    初期コミット

---
+++ Source/FPS/FPSGameModeBase.h
@@ -0,0 +1,18 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPS_API AFPSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	virtual void StartPlay() override;
+	
+};
